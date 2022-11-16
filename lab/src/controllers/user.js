2a94894132c3bbf0f863fbb332a0085b128d9888
yGrouzelle <yan.grouzelle@edu.ece.fr>
2022-11-16 10:28:01 +0100

    g

---
+++ lab/src/controllers/user.js
@@ -0,0 +1,34 @@
+const db = require('../dbClient')
+
+module.exports = {
+  create: (user, callback) => {
+    // Check parameters
+    if(!user.username)
+      return callback(new Error("Wrong user parameters"), null)
+    // Create User schema
+    const userObj = {
+      firstname: user.firstname,
+      lastname: user.lastname,
+    }
+    // Save to DB
+    // TODO check if user already exists
+    db.hmset(user.username, userObj, (err, res) => {
+      if (err) return callback(err, null)
+      callback(null, res) // Return callback
+    })
+  },
+  // get: (username, callback) => {
+  //   // TODO create this method
+  // }
+  get: (username, callback) => {
+    if(!username)
+      return callback(new Error("Username must be provided"), null)
+    db.hgetall(username, function(err, res) {
+      if (err) return callback(err, null)
+      if (res)
+        callback(null, res)
+      else
+        callback(new Error("User doesn't exists"), null)
+    })
+  }
+}
